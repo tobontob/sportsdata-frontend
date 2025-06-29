f9e0f453b54734d19eb23d72448ed605c8a54055
tobontob <tobontob@naver.com>
2025-06-30 01:06:31 +0900

    Update socket.ts

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,6 +1,6 @@
-import { io, Socket } from 'socket.io-client'
+import io from 'socket.io-client'
 
-const socket: Socket = io(process.env.NEXT_PUBLIC_SOCKET_URL || 'http://localhost:5000', {
+const socket = io(process.env.NEXT_PUBLIC_SOCKET_URL || 'http://localhost:5000', {
   transports: ['websocket'],
   autoConnect: true,
   reconnection: true,
