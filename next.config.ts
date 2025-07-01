088f285faabead11ba3550b35284cc2255c6a759
tobontob <tobontob@naver.com>
2025-07-02 00:37:43 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://web-production-190c.up.railway.app/api/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
